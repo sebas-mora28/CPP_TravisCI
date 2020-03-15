2a74e9fee5a27b869bb41a85388bf47e8c51283d
sebas-mora28 <semasmora@gmail.com>
2020-03-14 21:51:43 -0600

    Configuration

---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main() {
+    return 0;
+}
+
+class Calculator{
+
+    int adding(int number, int number2){
+        return number + number2;
+    }
+
+    int substraction(int number, int number2){
+        return number + number2;
+    }
+
+    int multiplication(int number, int number2){
+        return number + number2;
+    }
+
+    int division(int number, int number2){
+        return number + number2;
+    }
+};
