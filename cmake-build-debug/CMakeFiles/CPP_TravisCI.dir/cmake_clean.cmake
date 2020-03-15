e12632db58572d627a98885ef94a42c0abc70cd0
sebas-mora28 <semasmora@gmail.com>
2020-03-14 22:19:32 -0600

    Configuration

---
+++ cmake-build-debug/CMakeFiles/CPP_TravisCI.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CPP_TravisCI.dir/src/main.cpp.o"
+  "CPP_TravisCI"
+  "CPP_TravisCI.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CPP_TravisCI.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
