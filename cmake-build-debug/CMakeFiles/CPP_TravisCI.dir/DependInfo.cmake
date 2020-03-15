7bbecc1d10d5dd3907dcd67a01b331b0aad775bb
sebas-mora28 <semasmora@gmail.com>
2020-03-14 23:23:30 -0600

    Sonar Configuration

--- cmake-build-debug/CMakeFiles/CPP_TravisCI.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/CPP_TravisCI.dir/DependInfo.cmake
@@ -5,6 +5,7 @@ set(CMAKE_DEPENDS_LANGUAGES
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
   "/home/sebasmora/Documents/TEC/Semestre III/Datos II/Actividades/CPP_TravisCI/src/Calculator.cpp" "/home/sebasmora/Documents/TEC/Semestre III/Datos II/Actividades/CPP_TravisCI/cmake-build-debug/CMakeFiles/CPP_TravisCI.dir/src/Calculator.cpp.o"
+  "/home/sebasmora/Documents/TEC/Semestre III/Datos II/Actividades/CPP_TravisCI/src/main.cpp" "/home/sebasmora/Documents/TEC/Semestre III/Datos II/Actividades/CPP_TravisCI/cmake-build-debug/CMakeFiles/CPP_TravisCI.dir/src/main.cpp.o"
   "/home/sebasmora/Documents/TEC/Semestre III/Datos II/Actividades/CPP_TravisCI/test/calculator.test.cpp" "/home/sebasmora/Documents/TEC/Semestre III/Datos II/Actividades/CPP_TravisCI/cmake-build-debug/CMakeFiles/CPP_TravisCI.dir/test/calculator.test.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
